4843473ac0454c192331409369b72792751dccd2
Phillip Whittingham <phillip.whittingham@gmail.com>
2023-09-17 17:42:40 +0200

    Add char counting to the route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.domain import CharCounter
 
 from app.dto import ResponseDto, CommandDto
 
@@ -8,4 +9,16 @@ app = FastAPI()
 
 @app.post("/do-thing", response_model=ResponseDto)
 def do_a_thing_url_command(command: CommandDto):
-    return ResponseDto(input=command, count=0)
+    command_string = command.command
+
+    character_counter = CharCounter(input=command_string)
+
+    char_counts = character_counter.count_characters()
+
+    first_character = command_string[0]
+
+    first_character_counts = [
+        char_count for char_count in char_counts if char_count.char == first_character
+    ]
+
+    return ResponseDto(input=command, count=first_character_counts[0].count)
