12d60f611e4075a71aa93d6e4213c8c8a65a1973
Phillip Whittingham <phillip.whittingham@gmail.com>
2023-09-18 21:54:21 +0200

    Add repository for storing computed character counts

---
+++ app/repository.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+from pydantic import BaseModel
+
+from app.domain import CharCount
+
+
+class CharacterCountRepository(BaseModel):
+    # Ignore typing for this example, but imagine this could be
+    # your typical pymongo collection or some sqlalchemy construct
+    # which allows for saving and retrieving
+    client: Any
+
+    def get_count(self, character: str, request_input: str) -> int:
+        database_counts = self.client.find({"request_input": request_input})
+
+        for database_count in database_counts:
+            if database_count["count"]["char"] != character:
+                continue
+
+            return database_count["count"]["count"]
+
+    def save_count(self, character_count: CharCount, request_input: str):
+        database_character_count = {
+            "count": character_count,
+            "request_input": request_input,
+        }
+        self.client.insert_one(database_character_count)
---
+++ test/unit/test_repository.py
@@ -0,0 +1,27 @@
+from unittest.mock import MagicMock
+from app.domain import CharCount
+from app.repository import CharacterCountRepository
+
+
+def test_character_count_repository_save_with_inputs_returns_none():
+    char_count = CharCount(char="t", count=1)
+    request_input = "this"
+
+    mocked_client = MagicMock()
+    mocked_client.insert_one.return_value = None
+    repository = CharacterCountRepository(client=mocked_client)
+
+    # We assert the function completed successfully
+    assert repository.save_count(char_count, request_input) is None
+
+
+def test_character_count_repository_get_with_count_present_returns_count_value():
+    mocked_client = MagicMock()
+    mocked_client.find.return_value = [
+        {"request_input": "this", "count": {"char": "t", "count": 1}}
+    ]
+    repository = CharacterCountRepository(client=mocked_client)
+
+    count = repository.get_count("t", "this")
+
+    assert count == 1
